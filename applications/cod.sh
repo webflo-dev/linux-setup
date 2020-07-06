d5c53e2fdcf1f74c60b6e3fffce3724dd4ecc001
Florent <6934017+webflo-dev@users.noreply.github.com>
2020-07-06 23:29:48 +0200

    add cod

---
+++ applications/cod.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+declare repo=dim-an/cod
+declare version=$(get_latest_release_github $repo)
+declare file=cod-Linux.tgz
+declare tmp_dir=$tempdir/cod
+
+download_file \
+    https://github.com/$repo/releases/download/$version/$file \
+    $file
+
+mkdir -p \
+    $tmp_dir \
+    $homedir/.zsh
+
+tar xvf $tempdir/$file -C $tmp_dir --strip-components=1
+cp -f $tmp_dir/cod $bindir
+
+$bindir/cod init $$ zsh >$homedir/.zsh/cod.zsh
