b9120c7f1b7fbd9b228893e5ff4480bd1fa02a4b
Florent <6934017+webflo-dev@users.noreply.github.com>
2020-07-06 23:50:13 +0200

    add z

---
+++ applications/z.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+declare repo=rupa/z
+declare version=$(get_latest_release_github $repo)
+declare file=z-$version.tar.gz
+declare tmp_dir=$tempdir/z
+
+download_file \
+    https://github.com/$repo/archive/$version.tar.gz \
+    $file
+
+mkdir -p \
+    $tmp_dir \
+    $homedir/.zsh
+
+tar xvf $tempdir/$file -C $tmp_dir --strip-components=1
+cp -f $tmp_dir/z.sh $homedir/.zsh/z.zsh
+cp -f $tmp_dir/z.1 /usr/local/man/man1
