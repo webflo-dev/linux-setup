064e5bcc7f90d288c3a7af94082fd659061a630d
Florent <6934017+webflo-dev@users.noreply.github.com>
2020-07-06 18:19:59 +0200

    add h

---
+++ applications/h.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+declare hdir=${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/h
+
+unalias h
+
+# dependencies
+aptx install ack-grep
+
+git clone https://github.com/paoloantinori/hhighlighter.git $hdir
+mv $hdir/h.sh $hdir/h.plugin.zsh
