d46071085759bf0fd8d8413b71050c7ee15d2a06
Florent <6934017+webflo-dev@users.noreply.github.com>
2020-07-07 10:00:06 +0200

    add delta

---
+++ applications/delta.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+declare repo=dandavison/delta
+declare version=$(get_latest_release_github $repo)
+declare file=git-delta_${version}_amd64.deb
+
+download_file \
+    https://github.com/$repo/releases/download/$version/$file \
+    $file
+
+aptx install $tempdir/$file
+
+cat $appsdir/delta.config.txt >>$homedir/.gitconfig
